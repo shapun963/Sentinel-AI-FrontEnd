75ee1ea920f70c372594630aca12e0e98c586b66
Shapun S Poonja <71916205+shapun963@users.noreply.github.com>
2025-07-05 04:54:48 +0000

    fix: update config

--- src/conf.tsx
+++
@@ -1,6 +0,0 @@
-
-// src/config.js
-export const USE_MOCK_DATA = true; // Set to false to use real APIs
-
-export const SHIELD_SERVER_URL = "http://<your-shield-server-ip>:5001";
-export const AGENT_SERVER_URL = "http://<your-agent-server-ip>:5002";
---
+++ src/config.js
@@ -0,0 +1,5 @@
+// src/config.js
+export const USE_MOCK_DATA = true; // Set to false to use real APIs
+
+export const SHIELD_SERVER_URL = "localhost::5001";
+export const AGENT_SERVER_URL = "localhost:5002";
